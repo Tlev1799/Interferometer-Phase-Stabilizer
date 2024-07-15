1e16b023986b17f4f4c6c312c633bc6d4363c34d
Tlev1799 <Tal19991@gmail.com>
2024-07-15 14:14:00 +0300

    Added skeleton for engine_controller

--- engine_controller.py
+++ engine_controller.py
@@ -1,1 +1,12 @@
-# TODO: Implement
+class engine_controller:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def mov_rel(self, x, y):
+        pass
+
+    def close(self):
+        pass
