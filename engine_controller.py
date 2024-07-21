3552c25f8173afcf107b6f9efe2ab5fab0613ff8
FarbD24 <daniilfarbshtein24@gmail.com>
2024-07-21 14:24:46 +0300

    Added inital engine code

--- engine_controller.py
+++ engine_controller.py
@@ -1,14 +1,36 @@
+from pipython import GCSDevice
+
 class engine_controller:
-    def __init__(self):
-        x=4
-        c=x+5
+    def __init__(self, pixel_coordinates):
+        self.pidevice = None
+        self.new_pixel_coordinates = pixel_coordinates
+        self.old_coordinates = [0,0]
+        self.eng_coordinates = None
+        self.relative_moving = None
         pass
 
     def connect(self):
-        pass
+        self.pidevice = GCSDevice('E-873')
+        self.pidevice.InterfaceSetupDlg()
+        print(self.pidevice.qIDN())
+        
+    
+    
+    def engine_coordinates(self):
+        self.eng_coordinates = self.pidevice.qPOS('AXIS_1','AXIS_2')
+        print(self.eng_coordinates)
 
-    def mov_rel(self, x, y):
-        pass
+
+    def mov_rel(self):
+        self.relative_moving = self.new_pixel_coordinates - self.old_coordinates
+        self.old_coordinates = self.new_pixel_coordinates
+        return self.relative_moving
+
+
+    def move_engine(self):
+        self.eng_coordinates = self.eng_coordinates + self.relative_moving
+        self.pidevice.MOV({'AXIS_1': self.eng_coordinates[0], 'AXIS_2': self.eng_coordinates[1]})        
 
     def close(self):
-        pass
+        self.pidevice.CloseConnection()
+        
